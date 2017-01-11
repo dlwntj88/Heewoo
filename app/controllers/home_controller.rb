c2258dfca82594eac43df39dd14e31b87d11d232
Heewoo <dlwntj88@gmail.com>
2017-01-11 07:36:31 +0000

    second commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ApplicationController
   def index
     
   end
+  def sum
+    @sum = 1+1
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'blogs' => 'blogs#index'
+  post 'blogs/create' => 'blogs#create'
+  get 'blogs/new' => 'blogs#new'
+  get 'posts/:id' => 'blogs#show'
   root 'home#index'
+  get 'posts/edit/:id' => 'blogs#edit'
+  put 'posts/update' => 'blogs#update'
+  get 'posts/delete/:id' => 'blogs#delete'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
